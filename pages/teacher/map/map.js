d9e2ca30e395d3643480a8ef8614cba92349b4f2
unknown <liufulin90@163.com>
2017-07-06 23:48:53 +0800

    update

--- pages/teacher/map/map.js
+++ pages/teacher/map/map.js
@@ -9,8 +9,14 @@ Page({
    * 页面的初始数据
    */
   data: {
+    array: ['美国', '中国', '巴西', '日本'],
   },
-
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -39,7 +45,7 @@ Page({
       fgColor: '#000000',
       canvasid: 'myCanvas'
     }
-    QrCode().toImg('test', options);
+    //QrCode().toImg('test', options);
   },
 
   /**
